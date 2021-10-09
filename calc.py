77cc0148baba21a57c3fe3357cf4bc5f3e39b97c
Richard Smid <richardsiegfriedsmid@gmail.com>
2021-10-09 21:59:44 +0000

    Removed commented shit added, changed line 28.

--- calc.py
+++ calc.py
@@ -4,8 +4,6 @@ import time
 
 client = discord.Client()
 
-# @bot.command(pass_context=True)
-
 @client.event
 async def on_ready():
     print('Connected as {0.user}'.format(client))
@@ -20,12 +18,9 @@ async def on_message(message):
         nums = message.content[5:].split()
         nums.append(str(int(nums[0]) + int(nums[1])))
         print(nums)
-        # f=open(str(message.author) + ".sh","w")
-        # f.write("#" + str(nums))
-        # f.write("\n !/bin/bash/ \n manim -pqh " + str(message.author) + ".py Output")
-        # f.close()
         g=open(str(message.author) + ".py","w")
-        g.write('from manim import * \n \nclass SlantsExample(Scene): \n     def construct(self): \n         a = Text(str(' + nums[0] + ') + "+" + str(' + nums[1] + ') + "=" + str(' + nums[2] + '), font_size=90) \n         self.play(Create(a))')
+        g.write('from manim import * \n \nclass SlantsExample(Scene): \n     def construct(self): \n         nums = ' + str(nums))
+        g.write('\n     a = Text(nums[0] + "+" + nums[1] + "=" + nums[2]), font_size=90) \n         for i in range (len(a) - len(nums[2]), len(a)):\n           print(i)          self.play(Create(a))')
         g.close()
         os.system("manim -pqh " + str(message.author) + ".py Output")
         await message.channel.send("done")
@@ -36,9 +31,6 @@ async def on_message(message):
         time.sleep(1)
         os.system("rm -rf /root/mount/Manim/calcbot/Media/videos/" + str(message.author))
         os.system("rm -rf /root/mount/Manim/calcbot/" + str(message.author) + ".py")
-        # await bot.send_file()
-
-# create file message.author put in numbers 
 
 client.run('ODAyOTk0Mzc4OTQ3NjkwNTg2.YA3Uyg.BKELipsMKOoRbtZrHxRVrSP0--k')
 
