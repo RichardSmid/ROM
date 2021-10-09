a400e5e08f9b0c7ead39ba3ff15bed48110b09fb
Richard Smid <richardsiegfriedsmid@gmail.com>
2021-10-09 22:32:11 +0000

    Changed color of result from RED to TEAL in line 20.

--- calc.py
+++ calc.py
@@ -17,7 +17,7 @@ async def on_message(message):
     if message.content.startswith('/calc'):
         nums = message.content[5:]
         g=open(str(message.author) + ".py","w")
-        g.write("\nfrom manim import * \n \nclass SlantsExample(Scene): \n     def construct(self): \n            result = " + nums + "\n            a = Text('" + nums + "' + '=' + str(result), font_size=90)\n            for i in range (len(a) - len(str(result)), len(a)):\n                a[i].set_color(RED)\n            self.play(Create(a))")
+        g.write("\nfrom manim import * \n \nclass SlantsExample(Scene): \n     def construct(self): \n            result = " + nums + "\n            a = Text('" + nums + "' + '=' + str(result), font_size=90)\n            for i in range (len(a) - len(str(result)), len(a)):\n                a[i].set_color(TEAL)\n            self.play(Create(a))")
         g.close()
         os.system("manim -pqh " + str(message.author) + ".py Output")
         await message.channel.send("done")
