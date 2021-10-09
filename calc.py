029743cdb1696f1ddb947f9ef0851c26d4acc1b1
Richard Smid <richardsiegfriedsmid@gmail.com>
2021-10-09 22:04:11 +0000

    removed line 20 print(nums)

--- calc.py
+++ calc.py
@@ -17,7 +17,6 @@ async def on_message(message):
     if message.content.startswith('/calc'):
         nums = message.content[5:].split()
         nums.append(str(int(nums[0]) + int(nums[1])))
-        print(nums)
         g=open(str(message.author) + ".py","w")
         g.write('from manim import * \n \nclass SlantsExample(Scene): \n     def construct(self): \n         nums = ' + str(nums))
         g.write('\n     a = Text(nums[0] + "+" + nums[1] + "=" + nums[2]), font_size=90) \n         for i in range (len(a) - len(nums[2]), len(a)):\n           print(i)          self.play(Create(a))')
