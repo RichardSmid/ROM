54e68bd9a98f643e8da2faf9ea736194d642c03c
Richard Smid <richardsiegfriedsmid@gmail.com>
2021-10-09 22:43:01 +0000

    Changed animation from Create to Write line 20, removed done message line 23, removed Result: message line 26.

--- calc.py
+++ calc.py
@@ -17,13 +17,13 @@ async def on_message(message):
     if message.content.startswith('/calc'):
         nums = message.content[5:]
         g=open(str(message.author) + ".py","w")
-        g.write("\nfrom manim import * \n \nclass SlantsExample(Scene): \n     def construct(self): \n            result = " + nums + "\n            a = Text('" + nums + "' + '=' + str(result), font_size=90)\n            for i in range (len(a) - len(str(result)), len(a)):\n                a[i].set_color(TEAL)\n            self.play(Create(a))")
+        g.write("\nfrom manim import * \n \nclass SlantsExample(Scene): \n     def construct(self): \n            result = " + nums + "\n            a = Text('" + nums + "' + '=' + str(result), font_size=90)\n            for i in range (len(a) - len(str(result)), len(a)):\n                a[i].set_color(TEAL)\n            self.play(Write(a))")
         g.close()
         os.system("manim -pqh " + str(message.author) + ".py Output")
-        await message.channel.send("done")
+
         time.sleep(3)
         print("/root/mount/Manim/calcbot/Media/videos/" + str(message.author) + "/1080p60/SlantsExample.mp4")
-        await message.channel.send('Result: ', file =discord.File( "/root/mount/Manim/calcbot/Media/videos/" + str(message.author) + "/1080p60/SlantsExample.mp4"))
+        await message.channel.send('', file =discord.File( "/root/mount/Manim/calcbot/Media/videos/" + str(message.author) + "/1080p60/SlantsExample.mp4"))
         # os.rmdir("/root/mount/manim/calcbot/media/videos/" + str(message.author))
         time.sleep(1)
         os.system("rm -rf /root/mount/Manim/calcbot/Media/videos/" + str(message.author))
