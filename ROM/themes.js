4d0a5be4be4479168fcce5a1ea8e9042515fb541
Ondym <ondramat007@gmail.com>
2021-10-16 12:01:48 +0200

    creating cookie theme system all lines

--- ROM/themes.js
+++ ROM/themes.js
@@ -1,3 +1,10 @@
-function set() {
+function setTheme(themeID) {
+    document.cookie = "theme:" + themeID;
+    
+}
+
+function loadTheme() {
+    let currentTheme = parseInt(document.cookie.substring(5, document.cookie - 1));
+    let root = document.documentElement;
     
 }
