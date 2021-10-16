09e87c3983fafdbc986a2b162ac2d72820a73fbd
Ondym <ondramat007@gmail.com>
2021-10-16 21:09:10 +0200

    blank

--- ROM/menu.js
+++ ROM/menu.js
@@ -1,5 +1,4 @@
 const content = [
-
     "Lorem",
     "Ipsum",
     [
@@ -27,3 +26,18 @@ function makeContent(item, parent) {
 }
 
 makeContent(content, document.getElementsByClassName("menu")[0]);
+
+//          THEMES
+
+const themes = {
+Blueish: ['rgb(46, 132, 166)', 'rgb(10, 166, 122)', 'rgb(147, 191, 175)', 'rgb(217, 197, 173)', 'rgb(64, 64, 64)'],
+Redish: ['rgb(3, 140, 101)', 'rgb(242, 235, 223)', 'rgb(242, 129, 29)', 'rgb(242, 5, 5)', 'rgb(140, 3, 3)'],
+Yellowish: ['rgb(17, 33, 64)', 'rgb(50, 61, 64)', 'rgb(242, 229, 48)', 'rgb(166, 159, 65)', 'rgb(242, 211, 53)'],
+}
+
+for (const iterator of object) {
+    
+}
+function addTheme() {
+    
+}
