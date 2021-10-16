47aee637232f8236545a58058410398367a6df89
Ondym <ondramat007@gmail.com>
2021-10-16 11:20:11 +0200

    menu.js fixed the generation system all lines menu.js deleted unnecesary lines 14,19, 22, 26,33 style.css edited menu styling style.css styled h1 in content 33:37

--- ROM/menu.js
+++ ROM/menu.js
@@ -11,23 +11,19 @@ const content = [
     "Document",
     "~10"
 ]
-const tag = document.getElementsByClassName("menu")[0];
 
 function makeContent(item, parent) {
     console.log(item);
     if (Array.isArray(item)) {
-        // tag.innerHTML += "<ul>";
         let ul = document.createElement("ul");
         ul.innerHTML += "<li><h2>" + item[0] + "</h2></li>";   
-        for (let i = 0; i < item.length; i++) {
+        for (let i = 1; i < item.length; i++) {
             makeContent(item[i], ul);
         }
         parent.appendChild(ul);
-        // tag.innerHTML += "</ul>";
     } else {
         parent.innerHTML += "<li>" + item + "</li>";
     }
 }
 
-makeContent(content, tag);
-// document.getElementsByClassName("menu")[0].appendChild(tag);
+makeContent(content, document.getElementsByClassName("menu")[0]);
