d6a7b515ec3119001554a61b18aaae2b7d677701
Richard Smid <richardsiegfriedsmid@gmail.com>
2021-10-18 19:22:59 +0200

    Removed space bar line 5 manim\scitani-odcitani-komutativni-zakon

--- Manim/scitani-odcitani-komutativni-zakon/scene.py
+++ Manim/scitani-odcitani-komutativni-zakon/scene.py
@@ -2,5 +2,5 @@ from manim import *
 
 class komutativnizakon(Scene):
     def construct(self):
-        text = MarkupText(f'3+5<span fgcolor="#038C65">=</span> <span fgcolor="#F20505">8</span>', color="#F2EBDF")
+        text = MarkupText(f'3+5<span fgcolor="#038C65">=</span><span fgcolor="#F20505">8</span>', color="#F2EBDF")
         self.play(Write(text))
