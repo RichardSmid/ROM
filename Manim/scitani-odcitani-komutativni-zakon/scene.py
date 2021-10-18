50e324382e2e5e006ad46e33cfa86996f6c08772
Richard Smid <richardsiegfriedsmid@gmail.com>
2021-10-18 16:26:30 +0200

    Removed commented out text in manim/scitani-odcitani-komutativni-zakon

--- Manim/scitani-odcitani-komutativni-zakon/scene.py
+++ Manim/scitani-odcitani-komutativni-zakon/scene.py
@@ -2,19 +2,6 @@ from manim import *
 
 class komutativnizakon(Scene):
     def construct(self):
-        text = MarkupText(f'3+5<span fgcolor="{038C65}">=</span> <span fgcolor="{#F20505}">8</span>', color=#F2EBDF)
+        text = MarkupText(f'3+5<span fgcolor="#038C65">=</span> <span fgcolor="#F20505">8</span>', color=#F2EBDF)
         self.play(Write(text))
 
-
-
- 
-        # framebox1 = SurroundingRectangle(text[1], buff = .1)
-        # framebox2 = SurroundingRectangle(text[3], buff = .1)
-        # self.play(
-            # Create(framebox1),
-        # )
-        # self.wait()
-        # self.play(
-            # ReplacementTransform(framebox1,framebox2),
-        # )
-        # self.wait()
