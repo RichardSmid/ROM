d4dfdc6332e1618fc2f6fd0056fe78c91e5331e2
Richard Smid <richardsiegfriedsmid@gmail.com>
2021-10-18 16:54:12 +0200

    Added bracket line 6 in manim\scitani-odcitani-komutativni-zakon

--- Manim/scitani-odcitani-komutativni-zakon/scene.py
+++ Manim/scitani-odcitani-komutativni-zakon/scene.py
@@ -3,4 +3,4 @@ from manim import *
 class komutativnizakon(Scene):
     def construct(self):
         text = MarkupText(f'3+5<span fgcolor="#038C65">=</span> <span fgcolor="#F20505">8</span>', color=#F2EBDF)
-        self.play(Write(text)
+        self.play(Write(text))
