329da3666ed7451745dd5dc391b7c2e732242e29
Richard Smid <richardsiegfriedsmid@gmail.com>
2021-10-23 19:15:06 +0200

    komutativni zakon scene.py created transform animation

--- Manim/scitani-odcitani-komutativni-zakon/scene.py
+++ Manim/scitani-odcitani-komutativni-zakon/scene.py
@@ -1,6 +1,10 @@
+from typing import Text
 from manim import *
 
 class komutativnizakon(Scene):
     def construct(self):
-        text = MarkupText(f'3+5<span fgcolor="#038C65">=</span><span fgcolor="#F20505">8</span>', color="#F2EBDF")
-        self.play(Write(text))
+        first = MarkupText(f'3+5<span fgcolor="#038C65">=</span><span fgcolor="#F20505">8</span>', color="#F2EBDF")
+        second = MarkupText(f'5+3<span fgcolor="#038C65">=</span><span fgcolor="#F20505">8</span>', color="#F2EBDF")
+
+        self.play(Write(first))  # animate the creation of the square
+        self.play(Transform(first, second))  # interpolate the square into the circle
