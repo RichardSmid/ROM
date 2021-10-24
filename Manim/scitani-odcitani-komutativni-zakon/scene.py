a9668aa836b05b3a642152a9ad76931ae6570aef
Ondym <ondramat007@gmail.com>
2021-10-24 21:42:56 +0200

    rendered

--- Manim/scitani-odcitani-komutativni-zakon/scene.py
+++ Manim/scitani-odcitani-komutativni-zakon/scene.py
@@ -3,8 +3,8 @@ from manim import *
 
 class komutativnizakon(Scene):
     def construct(self):
-        first = MarkupText(f'3+5<span fgcolor="#038C65">=</span><span fgcolor="#F20505">8</span>', color="#F2EBDF")
-        second = MarkupText(f'5+3<span fgcolor="#038C65">=</span><span fgcolor="#F20505">8</span>', color="#F2EBDF")
+        first = MarkupText(f'3+5=<span fgcolor="#F20505">8</span>', color="#F2EBDF")
+        second = MarkupText(f'5+3=<span fgcolor="#F20505">8</span>', color="#F2EBDF")
 
         self.play(Write(first))
         self.play(Transform(first, second))
