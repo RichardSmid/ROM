cf8409e15ee26735b2d7b17f904d532891c7c263
Ondym <ondramat007@gmail.com>
2021-10-25 12:57:15 +0200

    created the image for sšítanec

--- Manim/scitani-odcitani/scitanec-soucet/scene.py
+++ Manim/scitani-odcitani/scitanec-soucet/scene.py
@@ -2,8 +2,20 @@ from manim import *
 
 class operation(Scene):
     def construct(self):
-        operation = MarkupText(f'2 + 4 = <span fgcolor="#F20505">6</span>', color="#F2EBDF", font_size=90).shift(UP * .45)
-        description = MarkupText(f'addend    addend   <span fgcolor="#F20505">sum</span>', color="#F2EBDF", font_size=40).shift(DOWN * .45)
+        two = Tex("2 ")
+        plus = Tex(" + ").next_to(two,RIGHT ,buff=SMALL_BUFF)
+        four = Tex("4").next_to(plus, RIGHT,buff=SMALL_BUFF)
+        equal = Tex("=").next_to(four, RIGHT,buff=SMALL_BUFF)
+        six = Tex("6").next_to(equal, RIGHT,buff=SMALL_BUFF).set_color(PURE_RED)
 
-        self.add(operation)     
-        self.add(description)     
+        operation = VGroup(two,plus,four,equal,six).scale(3)
+
+        added1 = Tex("sčítanec").next_to(two,DOWN,buff = SMALL_BUFF)
+        added2 = Tex("sčítanec").next_to(four,DOWN,buff = SMALL_BUFF)
+        sum = Tex("součet").next_to(six,DOWN,buff = SMALL_BUFF).set_color(PURE_RED)
+
+        description = VGroup(added1,added2,sum)
+
+        all = VGroup(description,operation)
+
+        self.add(all)
